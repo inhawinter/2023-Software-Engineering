460fb534fa2d5dc8a260a25e4a70b7ca1662e08e
inhawinter <inhawinter@gmail.com>
2023-03-18 16:57:39 +0900

    week03 isp before

---
+++ week03/src/isp/Pokemon.java
@@ -0,0 +1,31 @@
+package isp;
+
+public interface Pokemon {
+    void attack();
+    void defend();
+    void fly();
+    void swim();
+}
+
+class Pikachu implements Pokemon{
+    // violates ISP
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void defend() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
