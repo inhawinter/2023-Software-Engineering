f624bce659150215b8c55bc8864bf42563885762
inhawinter <inhawinter@gmail.com>
2023-05-06 11:32:33 +0900

    week10 singleton pattern 02

--- week10/src/kr/ac/inha/singleton/SingletonDemo.java
+++ week10/src/kr/ac/inha/singleton/SingletonDemo.java
@@ -1,16 +1,27 @@
 package kr.ac.inha.singleton;
 
 class Singleton{
-    public Singleton() {
+    private static Singleton singleton = new Singleton();
+    //public Singleton() {
+    private Singleton(){
         System.out.println("Only One!");
     }
 
+    public static Singleton getSingleton() {
+        return singleton;
+    }
 }
 public class SingletonDemo {
     public static void main(String[] args) {
-        Singleton s1 = new Singleton();
-        Singleton s2 = new Singleton();
-        System.out.println(s1);
-        System.out.println(s2);
+        System.out.println(Singleton.getSingleton());
+        Singleton s3 = Singleton.getSingleton();
+        Singleton s4 = Singleton.getSingleton();
+        System.out.println(s3);
+        System.out.println(s4);
+
+//        Singleton s1 = new Singleton();
+//        Singleton s2 = new Singleton();
+//        System.out.println(s1);
+//        System.out.println(s2);
     }
 }
